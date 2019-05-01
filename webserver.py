cd14a830ea471ad267e2460a9875db97aa8f1214
Colm Faherty <colm29@gmail.com>
2019-05-01 22:16:16 +0100

    initial commit

---
+++ webserver.py
@@ -0,0 +1,79 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+
+# import CRUD Operations from Lesson 1
+from db_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurant_menu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+class webServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output += "<html><body><a href = 'restaurants/new'>Create New Restaurant</a></br>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    output += "<a href = #>Edit</a>"
+                    output += "</br>"
+                    output += "<a href = #>Delete</a>"
+                    output += "</br></br></br>"
+
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
+            if self.path.endswith("/restaurants/new"):
+                output = ""
+                self.send_response(200)
+                self.send_header('Content-type', 'text/HTML')
+                self.end_headers()
+                output += "<html><body><h1>Create a new Restaurant</h1>"
+                output += "<form method = 'POST' enctype = 'multipart/form-data' action = '/restaurants/new'>New Restaurant Name: <input type='text' name='resto'><br>"
+                output += "<input type='submit' value='Create'></form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(301)
+                self.end_headers()
+
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = parse_multipart(self.rfile,pdict)
+                    messagecontent = fields.get('resto')
+                    newResto = Restaurant(name = messagecontent)
+                    session.add(newResto)
+                    session.commit()
+
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+
+def main():
+    try:
+        server = HTTPServer(('', 8080), webServerHandler)
+        print 'Web server running...open localhost:8080/restaurants in your browser'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
