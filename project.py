804be4c08ea1e4384448d745dd4cdf0b85e377f9
Colm Faherty <colm29@gmail.com>
2019-05-05 20:02:39 +0100

    added template

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, Restaurant, MenuItem
@@ -18,16 +18,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+    return render_template('menu.html',restaurant = restaurant, items = items)
 
 # Task 1: Create route for newMenuItem function here
 #@app.route('/')
@@ -36,8 +27,9 @@ def newMenuItem(restaurant_id):
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
 
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/edit')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
